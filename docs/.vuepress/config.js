c9f14a86dfa9ac5a9c5640cf0b1d770a685baf2b
reiner850593913 <103234074+reiner850593913@users.noreply.github.com>
2022-05-24 16:33:27 +0800

    feat(*): 最后更新时间

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: 
  * @Date: 2022-05-24 15:38:20
  * @LastEditors: Do not edit
- * @LastEditTime: 2022-05-24 16:25:57
+ * @LastEditTime: 2022-05-24 16:31:18
  * @FilePath: \reiner-blog\docs\.vuepress\config.js
  * @Description: 配置文件
  */
@@ -29,6 +29,7 @@ module.exports = {
                     ]
                 }
             ]
-        }
+        },
+        lastUpdated: true
     }
 }
