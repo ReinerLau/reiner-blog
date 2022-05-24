bf5129099ff2e6c7c6e901cf7004475c89fcf272
reiner850593913 <103234074+reiner850593913@users.noreply.github.com>
2022-05-24 16:00:27 +0800

    feat(*): 左上角图标

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: 
  * @Date: 2022-05-24 15:38:20
  * @LastEditors: Do not edit
- * @LastEditTime: 2022-05-24 15:50:17
+ * @LastEditTime: 2022-05-24 15:59:26
  * @FilePath: \reiner-blog\docs\.vuepress\config.js
  * @Description: 配置文件
  */
@@ -14,4 +14,7 @@ module.exports = {
             { rel: 'icon', href: '/avator.jpg' }
         ],
     ],
+    themeConfig:{
+        logo:'/avator.jpg'
+    }
 }
